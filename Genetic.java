58efe68613dbacce8e7cfddd1e532ebec8afdd03
António Pereira <antonio_m_sp@hotmail.com>
2018-05-11 11:01:21 +0100

    started wheel

--- Genetic.java
+++ Genetic.java
@@ -1,165 +1,250 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 
 import jdk.nashorn.internal.runtime.Context.ThrowErrorManager;
 
 public class Genetic {
 
-	public static double getScore(Conference conf) {
-		
-		ArrayList<Integer> daysRepresentation = new ArrayList<Integer>();
 
-		double score = 0;
-		double reprs = 0;
-		for (Day d: conf.days) {
+	private ArrayList<Conference> initialPopulation;
+	private ArrayList<Conference> population;
 
-			ArrayList<Integer> papersRepresentations = new ArrayList<Integer>();
-			if (!d.checkPresenters()) {
-				System.out.println("merda1");
-				return 0;
-			}
-			for (Session s: d.sessions) {
-				if (s.getNumberOfFullPapers() < 2) {
-					System.out.println("merda2");
-					return 0;
-				}
-				
-				for (Paper p : s.getPapers()) {
-					if (!p.checkIfPresenterIsAuthor()){
-						System.out.println("merda3");
-						return 0;
-					}					
-				}
-				score += (s.checkThemesID() * Utilities.THEM_WEIGHT);
-				
-				papersRepresentations.add(s.getDuration());
-				
-			}
-			int repr = calculateRepresentation(papersRepresentations);
-		//	System.out.println("difference in sessions of a day: " + repr);
-			reprs += 1.0 - repr/7.0;
-		//	System.out.println("reprs: " + reprs);
-			daysRepresentation.add(repr);
+
+	public Genetic(ArrayList<Conference> initialPopulation) {
+
+		this.initialPopulation = initialPopulation;
+
+		selection();
+
+
+	}
+
+
+	public void selection() {
+		Utilities.SELECTION selection_t = Utilities.SELECTION.ELITIST;
+
+		//best n elements -> input asked
+		int N = 2;
+
+		//if elitist
+		switch(selection_t) {
+		case ELITIST :
+			population = elitist(N);
+			break;
+
+		case PROBABILISTIC :
+			population = probabilistic();
+			break;
 		}
-		reprs*= Utilities.DAY_WEIGHT;
-		int rep = calculateRepresentation(daysRepresentation);
-		//System.out.println("day rep: " + rep);
-		score += (1.0 - rep/7.0)*Utilities.CONF_WEIGHT;
+
+	}
+
+
+	private ArrayList<Conference> probabilistic() {
+
+		double totalSum = 0;
+
+		for (Conference c : this.initialPopulation)
+			totalSum += c.getScore();
+
+		for (Conference c : this.initialPopulation)
+			c.setProbability( c.getScore()/totalSum);
 		
-		//System.out.println("final rep: " + reprs);
+		
 
-		return score + reprs;
+		return null;
 	}
 
-	private static int calculateRepresentation(ArrayList<Integer> representations) {
-				
-		int min = representations.get(0);
-	    int max = min;
-	    int size = representations.size();
-        for (int i = 0; i < size; i++){
-        	
-        	int value = representations.get(i);
-            if (min > value)
-                min = value;
-            if (max < value)
-                max = value;
-        }
-        return max-min;
+
+	public ArrayList<Conference> elitist(int N) {
+
+		ArrayList<Conference> bestN = getNBestConfs(N);
+
+		return fillPopulation(bestN);
+
 	}
+
+
+	private ArrayList<Conference> getNBestConfs( int N) {		
+
+		initialPopulation.sort(Comparator.comparingDouble(Conference::getScore));
+
+		return (ArrayList<Conference>) initialPopulation.subList(0,N);
+	}
+
+
+	private ArrayList<Conference> fillPopulation(ArrayList<Conference> best) {
+
+
+		int div = Utilities.POPULATION_SIZE / best.size();
+		int module = Utilities.POPULATION_SIZE % best.size();
+
+		ArrayList<Conference> newPopulation = new ArrayList<Conference>();
+
+		for (int i = 0 ; i < div ; i++) 
+			newPopulation.addAll(best);
+
+		for (int i = 0 ; i < module ; i++)
+			newPopulation.add(best.get(i));
+
+
+		return newPopulation;
+
+	}
+
+
+
+	public ArrayList<Conference> wheel(ArrayList<Conference> population, boolean naturalFitnessScores,	int selectionSize,	Random rng)
+	{
+		// Record the cumulative fitness scores.  It doesn't matter whether the
+		// population is sorted or not.  We will use these cumulative scores to work out
+		// an index into the population.  The cumulative array itself is implicitly
+		// sorted since each element must be greater than the previous one.  The
+		// numerical difference between an element and the previous one is directly
+		// proportional to the probability of the corresponding candidate in the population
+		// being selected.
+		double[] cumulativeFitnesses = new double[population.size()];
+		cumulativeFitnesses[0] = getAdjustedFitness(population.get(0).getProbability(),
+				naturalFitnessScores);
+		for (int i = 1; i < population.size(); i++)
+		{
+			double fitness = getAdjustedFitness(population.get(i).getProbability(), naturalFitnessScores);
+			cumulativeFitnesses[i] = cumulativeFitnesses[i - 1] + fitness;
+		}
+
+		ArrayList<Conference> selection = new ArrayList<Conference>(selectionSize);
+		for (int i = 0; i < selectionSize; i++)
+		{
+			double randomFitness = rng.nextDouble() * cumulativeFitnesses[cumulativeFitnesses.length - 1];
+			int index = Arrays.binarySearch(cumulativeFitnesses, randomFitness);
+			if (index < 0)
+			{
+				// Convert negative insertion point to array index.
+				index = Math.abs(index + 1);
+			}
+			selection.add(population.get(index));
+		}
+		return selection;
+	}
+
+
+	private double getAdjustedFitness(double rawFitness, boolean naturalFitness)
+	{
+		if (naturalFitness)
+		{
+			return rawFitness;
+		}
+		else
+		{
+			// If standardised fitness is zero we have found the best possible
+			// solution.  The evolutionary algorithm should not be continuing
+			// after finding it.
+			return rawFitness == 0 ? Double.POSITIVE_INFINITY : 1 / rawFitness;
+		}
+	}
+
+
 	
+	
+	
+	
+
+
 	public static String crossCromossomes(Conference c1, Conference c2) {
-		
+
 		if (c1.cromossome.length() != c2.cromossome.length()) {
 			throw new java.lang.RuntimeException("Error! The cromossome's sizes are different.");
 		}
-		
+
 		String newCromossome = "";
 		int offset = 0;
-		
-//      DIA                (HORA   TEMA    DURA    (AP AA AA TT TT  full)  (papper2))           (HORA TEMA DUR		    (paper)   (paper2))
-//		String cromo =  "00  11   11      001    00 00 01  10 11  1    01011110110           01      00 11 001 00000110111  01011110110"; 
-//		String cromo2 = "01  01   01      000    01 10 00  00 10  1    01101110110           01      10 01 011 01010110101  11010111100";
-	
+
+		//      DIA                (HORA   TEMA    DURA    (AP AA AA TT TT  full)  (papper2))           (HORA TEMA DUR		    (paper)   (paper2))
+		//		String cromo =  "00  11   11      001    00 00 01  10 11  1    01011110110           01      00 11 001 00000110111  01011110110"; 
+		//		String cromo2 = "01  01   01      000    01 10 00  00 10  1    01101110110           01      10 01 011 01010110101  11010111100";
+
 		System.out.println("Total lenght: " + c1.cromossome.length());
-		
+
 		for (int days = 0 ; days < Utilities.DAYS ; days ++) {
 			newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.DAYS), c2.cromossome.substring(offset, offset+Utilities.DAYS));
 			offset += Utilities.DAYS;
-			
+
 			for (int sessions = 0 ; sessions < Utilities.SESSIONS_PER_PERIOD ; sessions++) {
 				// offset + 2 porque o número de bits que representam o periodo é 2
 				newCromossome += crossAux(c1.cromossome.substring(offset, offset+2), c2.cromossome.substring(offset, offset+2));
 				offset += 2;
 				System.out.println("current offset: " + offset);
-				
+
 				// Tema de sessao
 				newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.SESSION_THEME), c2.cromossome.substring(offset, offset+Utilities.SESSION_THEME));
 				offset += Utilities.SESSION_THEME;
 				System.out.println("current offset: " + offset);
-				
+
 				// duração da sessao
 				newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.DURATION), c2.cromossome.substring(offset, offset+Utilities.DURATION));
 				offset += Utilities.DURATION;
 				System.out.println("current offset: " + offset);
-				
+
 				for (int paper = 0 ; paper < Utilities.PAPERS_PER_SESSION ; paper++) {
 					// Presenter
 					newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.PRESENTER), c2.cromossome.substring(offset, offset+Utilities.PRESENTER));
 					offset += Utilities.PRESENTER;
 					System.out.println("current offset: " + offset);
-					
+
 					for (int authors = 0 ; authors < Utilities.AUTHORS_PER_PAPER ; authors++) {
 						// Authors
 						newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.AUTHORS), c2.cromossome.substring(offset, offset+Utilities.AUTHORS));
 						offset += Utilities.AUTHORS;						
 					}
-					
+
 					for (int themes = 0 ; themes < Utilities.THEMES_PER_PAPER; themes++) {
 						// Themes
 						newCromossome += crossAux(c1.cromossome.substring(offset, offset+Utilities.THEME), c2.cromossome.substring(offset, offset+Utilities.THEME));
 						offset += Utilities.THEME;						
 					}
-					
+
 					// Is full paper (defined always by 1 bit)
 					newCromossome += crossAux(c1.cromossome.substring(offset, offset+1), c2.cromossome.substring(offset, offset+1));
 					offset += 1;
-					
+
 				}
-				
-				
+
+
 			}
-//			offset += Utilities.DAY;
+			//			offset += Utilities.DAY;
 		}
-		
+
 		System.out.println("offset: "+ offset);
-		
+
 		Utilities.getCromossomeSize();
-		
+
 		if (c1.cromossome.length() != offset) {
 			throw new java.lang.RuntimeException("Error! The cromossome and the generated cromossome size are different.");
 		}
-		
+
 		System.out.println("New Cromossome: " + newCromossome);
 		return "";
 	}
-	
+
 	public static String crossAux(String s1, String s2) {
 		Random rand = new Random();
 		int n = rand.nextInt(101);
-		
+
 		if (n < Utilities.CROSSING_RATIO * 100) {
 			return " " + s1;
 		} else {
 			return " " + s2;
 		}
 	}
-	
+
 	public static String generateRandomPoplation() {
 		String newCromossome = "";
-		
+
 		Random rand = new Random();
 		int n = rand.nextInt(101);
-		
+
 		for (int i = 0 ; i < Utilities.getCromossomeSize() ; i++) {
 			if (n < 50) {
 				newCromossome += "0";
@@ -167,16 +252,7 @@ public class Genetic {
 				newCromossome += "1";
 			}
 		}
-		
+
 		return newCromossome;
 	}
 }
-
-
-
-
-
-
-
-
-
