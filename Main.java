9c14a7cf3aa1eb85232cc851fe50844d13ee90ae
Anderson Gralha <anderson.gralha@gmail.com>
2018-05-20 10:33:56 +0100

    commit

--- Main.java
+++ Main.java
@@ -6,9 +6,21 @@ import java.util.Random;
 public class Main {
 	public static void main(String[] args) {
 
-
 		
-		/*
+		ArrayList<Conference> p = new ArrayList<Conference>();
+
+		for (int i = 0; i < Utilities.POPULATION_SIZE ; i++) {
+			p.add(new Conference(Genetic.generateRandomPopulation()));
+		}
+	
+		Genetic g = new Genetic(p);	
+
+	}
+}
+
+/*
+ * 
+ * /*
 		 * REPRODU��O Os selecionados sao emparelhados aleatoriamente Por par, com
 		 * Pcrossover, pelo menos 1 crossover point � escolhido, de 1 a
 		 * N(Cromossoma.size()) Troca-se o material genetico
@@ -64,116 +76,63 @@ public class Main {
 //
 //		String cromo2 = "01111100100000110110110000100100110011";
 //		Conference c3 = new Conference(cromo2);
-		
-		
-		ArrayList<Conference> p = new ArrayList<Conference>();
+ 
+
+//DIA    HORA   TEMA    DURA    AP AA AA	TT TT  full  papper2             Sessao 2		     paper 2
+//String cromo = "01 -  11      11       001    00 00 01  10 11  1          			     01 11 001 00000110111  01011110110 
+//
+//		String cromo = "00  11   11      001    00 00 01  10 11  1    01011110110         01     00 11 001 00000110111  01011110110"; 
+//		Conference c1 = new Conference(cromo);
+//		System.out.println(c1);
 //		
+//		String cromo2 = "01  01   01      000    01 10 00  00 10  1    01101110110        01      10 01 011 01010110101  11010111100"; 
+//		Conference c2 = new Conference(cromo2);
+//		System.out.println(c2);
 //		
-		for (int i = 0; i < Utilities.POPULATION_SIZE ; i++) {
-			p.add(new Conference(Genetic.generateRandomPopulation()));
-		}
+//		Genetic.crossCromossomes(c1, c2);
+
+
+//		Conference c3 = new Conference(Genetic.generateRandomPoplation());
+//		Conference c4 = new Conference(Genetic.generateRandomPoplation());
 //		
-//		for (int i = 0; i < 100 ; i++) {
-//			System.out.println(p.get(i).getCromossome());
-//			}
-//		
-//		
-		
-		
-		Genetic g = new Genetic(p);
+//		Genetic.crossCromossomes(c3, c4);
 
 
-		//                DIA    HORA   TEMA    DURA    AP AA AA	TT TT  full  papper2             Sessao 2		     paper 2
-		//String cromo = "01 -  11      11       001    00 00 01  10 11  1          			     01 11 001 00000110111  01011110110 
-		//
-		//		String cromo = "00  11   11      001    00 00 01  10 11  1    01011110110         01     00 11 001 00000110111  01011110110"; 
-		//		Conference c1 = new Conference(cromo);
-		//		System.out.println(c1);
-		//		
-		//		String cromo2 = "01  01   01      000    01 10 00  00 10  1    01101110110        01      10 01 011 01010110101  11010111100"; 
-		//		Conference c2 = new Conference(cromo2);
-		//		System.out.println(c2);
-		//		
-		//		Genetic.crossCromossomes(c1, c2);
+//		ArrayList<Conference> arr = new ArrayList<Conference>();
+//		arr.add(c1);
+//		arr.add(c2);
+
+//		arr.sort(Comparator.comparingDouble(Conference::getScore));
+
+//		System.out.println("score final: " + Genetic.getScore(c1));
+// c1.calculateScore();
+// System.out.println("Score final: " + c1.getScore());
+
+// <DIA> <SESS�ES> <DIA> <SESS�ES> <DIA> <SESS�ES>, para 3 dias
+// * Ex:
+// * 00 01010101010 01 00000000000 10 11101010001
+// <SESSOES>
+// TT DD PPPPPP PPPPPPP
+
+/*
+* THE REAL DEAL NOW
+* 
+* Indiv�duo : uma solu��o que representa TODOS os dias e todas as sess�es
+* associadas
+* 
+* <DIA> <SESS�ES> <DIA> <SESS�ES> <DIA> <SESS�ES>, para 3 dias Ex: 00
+* 01010101010 01 00000000000 10 11101010001
+* 
+* 
+* <Sess�es>: XX YYYYYYYYY XX representa o hor�rio da sess�o(inicio/fim da
+* manh�/tarde) YYYY.. represneta o getGenome da classe session
+* 
+* 
+*/
 
 
-		//		Conference c3 = new Conference(Genetic.generateRandomPoplation());
-		//		Conference c4 = new Conference(Genetic.generateRandomPoplation());
-		//		
-		//		Genetic.crossCromossomes(c3, c4);
+/* --------------------------------------------*/
 
 
-		//		ArrayList<Conference> arr = new ArrayList<Conference>();
-		//		arr.add(c1);
-		//		arr.add(c2);
+//ask for user input
 
-		//		arr.sort(Comparator.comparingDouble(Conference::getScore));
-
-		//		System.out.println("score final: " + Genetic.getScore(c1));
-		// c1.calculateScore();
-		// System.out.println("Score final: " + c1.getScore());
-
-		// <DIA> <SESS�ES> <DIA> <SESS�ES> <DIA> <SESS�ES>, para 3 dias
-		// * Ex:
-		// * 00 01010101010 01 00000000000 10 11101010001
-		// <SESSOES>
-		// TT DD PPPPPP PPPPPPP
-
-		/*
-		 * THE REAL DEAL NOW
-		 * 
-		 * Indiv�duo : uma solu��o que representa TODOS os dias e todas as sess�es
-		 * associadas
-		 * 
-		 * <DIA> <SESS�ES> <DIA> <SESS�ES> <DIA> <SESS�ES>, para 3 dias Ex: 00
-		 * 01010101010 01 00000000000 10 11101010001
-		 * 
-		 * 
-		 * <Sess�es>: XX YYYYYYYYY XX representa o hor�rio da sess�o(inicio/fim da
-		 * manh�/tarde) YYYY.. represneta o getGenome da classe session
-		 * 
-		 * 
-		 */
-
-
-		/* --------------------------------------------*/
-
-
-		//ask for user input
-
-
-		ArrayList<Conference> population = new ArrayList<Conference>();
-
-
-
-		//populate population randomly
-
-
-
-		//create randomly cromossomes
-
-
-
-		//save score
-
-
-
-
-		ArrayList<Conference> arr = new ArrayList<Conference>(); 
-		ArrayList<Conference> arr2 = new ArrayList<Conference>();
-		
-	
-//		for (int i = 0; i < 100 ; i++) {
-//			arr.add(new Conference(Genetic.generateRandomPopulation()));
-//		}
-//		g.setCurrentPopulation(arr);
-		
-//		for (int i = 0; i < 100 ; i++) {
-//			arr2 = g.emparelhate();
-//		}
-////		
-//		for (int i = 0; i < 100 ; i++) {
-//			System.out.println(arr2.get(i).getCromossome());
-//		}
-	}
-}
